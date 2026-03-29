2fb070da4d1e68e57ffc5a3e2686afa938634582
lizzezzie <lizezzie@gmail.com>
2026-03-29 08:43:51 +0300

    bug fix

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,8 +1,8 @@
 const params = new URLSearchParams(window.location.search);
 
-document.getElementById("fname").textContent = params.get("fname");
-document.getElementById("lname").textContent = params.get("lname");
+document.getElementById("first name").textContent = params.get("first name");
+document.getElementById("last name").textContent = params.get("last name");
 document.getElementById("email").textContent = params.get("email");
-document.getElementById("phone").textContent = params.get("phone");
+document.getElementById("number").textContent = params.get("number");
 document.getElementById("business").textContent = params.get("business");
 document.getElementById("timestamp").textContent = params.get("timestamp");
