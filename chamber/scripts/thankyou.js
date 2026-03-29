141efb84c93bef00636934f85860d0a197ec74d0
lizzezzie <lizezzie@gmail.com>
2026-03-29 08:06:52 +0300

    thankyou js

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,8 @@
+const params = new URLSearchParams(window.location.search);
+
+document.getElementById("fname").textContent = params.get("fname");
+document.getElementById("lname").textContent = params.get("lname");
+document.getElementById("email").textContent = params.get("email");
+document.getElementById("phone").textContent = params.get("phone");
+document.getElementById("business").textContent = params.get("business");
+document.getElementById("timestamp").textContent = params.get("timestamp");
